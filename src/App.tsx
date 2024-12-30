6d9f5462b2ab57135facb6aed5ce19cd9af9b3e3
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-30 22:25:08 +0100

    Integrate useTimer hook and TimeControl component with session length functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.css";
 import { Footer } from "./components/Layout/footer";
 import { Header } from "./components/Layout/header";
-// import { TimeControl } from "./components/Timer/TimeControl";
+import { TimeControl } from "./components/Timer/TimeControl";
 import { useDarkMode } from "./hooks/useDarkMode";
+import { useTimer } from "./hooks/useTimer";
 
 function App() {
   const { isDark, setIsDark } = useDarkMode();
-  // const { breakLength } = useTimer();
+  const {
+    breakLength,
+    sessionLength,
+    timeLeft,
+    isRunning,
+    isSession,
+    audioRef,
+    incrementBreak,
+    decrementBreak,
+    incrementSession,
+    decrementSession,
+    toggleTimer,
+    reset,
+  } = useTimer();
   return (
     <div className="min-h-screen flex flex-col">
       <Header isDark={isDark} onToggleTheme={() => setIsDark(!isDark)} />
@@ -19,13 +33,32 @@ function App() {
           }`}
         ></div>
         <main className="container mx-auto px-4 py-8 flex items-center justify-center">
-          {/* <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+          <div className="min-h-screen bg-gray-100 flex items-center justify-center">
             <div className="bg-white p-8 rounded-lg shadow-xl max-w-md w-full">
               <div className="flex justify-around mb-8">
-                <TimeControl label="Break Length" value={breakLength} />
+                <TimeControl
+                  label="Break Length"
+                  value={breakLength}
+                  onIncrement={incrementBreak}
+                  onDecrement={decrementBreak}
+                  labelId="break-label"
+                  lengthId="break-length"
+                  incrementId="break-increment"
+                  decrementId="break-decrement"
+                />
+                <TimeControl
+                  label="session Length"
+                  value={sessionLength}
+                  onIncrement={incrementSession}
+                  onDecrement={decrementSession}
+                  labelId="session-label"
+                  lengthId="session-length"
+                  incrementId="session-increment"
+                  decrementId="session-decrement"
+                />
               </div>
             </div>
-          </div> */}
+          </div>
         </main>
       </div>
       <Footer />
